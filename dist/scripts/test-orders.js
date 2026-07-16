c16f82e099a0d221d9816e09a165f2d79dec2a1a
MohanAbhishek29 <mohanabhishek269@gmail.com>
2026-07-16 14:44:18 +0530

    chore: add debug display for allOrders to trace missing active order bug

---
+++ dist/scripts/test-orders.js
@@ -0,0 +1,6 @@
+const admin = require('firebase-admin');
+const fs = require('fs');
+const path = require('path');
+
+// Initialize Firebase Admin (assuming a service account key or default credentials work if emulator is used, but wait - there is no admin SDK setup here?)
+// We can use the firebase config if we use firebase-admin. Wait, I don't have the private key for dosa-house-7bd70!
